c46a7414f2d98b1fadeac315e4c5509a8d3c056e
Carlo Zapponi <littleark@gmail.com>
2020-06-21 15:42:16 +0000

    Resolve "Testing with Jest"

--- src/charts/chrtGrid/chrtGrid.js
+++ src/charts/chrtGrid/chrtGrid.js
@@ -1,12 +1,12 @@
-import { createSVG as create } from "~/layout";
-import { TICKS_DEFAULT } from "~/constants";
-import lineWidth from "./lib/lineWidth";
-import lineColor from "./lib/lineColor";
-import minor from "./lib/minor";
-import chrtGeneric from "../chrtGeneric";
+import { createSVG as create } from '~/layout';
+import { TICKS_DEFAULT } from '~/constants';
+import lineWidth from './lib/lineWidth';
+import lineColor from './lib/lineColor';
+import minor from './lib/minor';
+import chrtGeneric from '../chrtGeneric';
 
 const DEFAULT_LINE_WIDTH = 1;
-const DEAULT_LINE_COLOR = "#000";
+const DEAULT_LINE_COLOR = '#000';
 
 function chrtGrid(name, ticksNumber = TICKS_DEFAULT) {
   chrtGeneric.call(this);
@@ -18,19 +18,19 @@ function chrtGrid(name, ticksNumber = TICKS_DEFAULT) {
   this.showMinorTicks = false;
 
   const verticalGridLine = (gridLine, position, y1, y2, visible = true) => {
-    gridLine.style.display = visible ? "block" : "none";
-    gridLine.setAttribute("x1", position);
-    gridLine.setAttribute("x2", position);
-    gridLine.setAttribute("y1", y1);
-    gridLine.setAttribute("y2", y2);
+    gridLine.style.display = visible ? 'block' : 'none';
+    gridLine.setAttribute('x1', position);
+    gridLine.setAttribute('x2', position);
+    gridLine.setAttribute('y1', y1);
+    gridLine.setAttribute('y2', y2);
   };
 
   const horizontalGridLine = (gridLine, position, x1, x2, visible = true) => {
-    gridLine.style.display = visible ? "block" : "none";
-    gridLine.setAttribute("x1", x1);
-    gridLine.setAttribute("x2", x2);
-    gridLine.setAttribute("y1", position);
-    gridLine.setAttribute("y2", position);
+    gridLine.style.display = visible ? 'block' : 'none';
+    gridLine.setAttribute('x1', x1);
+    gridLine.setAttribute('x2', x2);
+    gridLine.setAttribute('y1', position);
+    gridLine.setAttribute('y2', position);
   };
 
   this.draw = () => {
@@ -43,28 +43,28 @@ function chrtGrid(name, ticksNumber = TICKS_DEFAULT) {
     const ticks = this.parentNode.scales[name].ticks(
       ticksNumber * (this.showMinorTicks ? 2 : 1)
     );
-    console.log("got this ticks", name, ticksNumber, ticks);
+    console.log('got this ticks', name, ticksNumber, ticks);
     ticks.forEach((tick) => {
       let gridLine = this.g.querySelector(
         `[data-id='gridLine-${name}-${tick.value}']`
       );
       if (!gridLine) {
-        gridLine = create("line");
-        gridLine.setAttribute("data-id", `gridLine-${name}-${tick.value}`);
+        gridLine = create('line');
+        gridLine.setAttribute('data-id', `gridLine-${name}-${tick.value}`);
 
         if (tick.isMinor) {
-          gridLine.classList.add("tick-minor");
+          gridLine.classList.add('tick-minor');
         }
 
         this.g.appendChild(gridLine);
       }
 
-      gridLine.setAttribute("stroke", this.stroke);
-      gridLine.setAttribute("stroke-width", this.strokeWidth);
+      gridLine.setAttribute('stroke', this.stroke);
+      gridLine.setAttribute('stroke-width', this.strokeWidth);
 
       const position = this.parentNode.scales[name](tick.value);
 
-      if (name === "x") {
+      if (name === 'x') {
         verticalGridLine(
           gridLine,
           position,
@@ -72,7 +72,7 @@ function chrtGrid(name, ticksNumber = TICKS_DEFAULT) {
           _margins.top
         );
       }
-      if (name === "y") {
+      if (name === 'y') {
         const isLog = this.parentNode.scales[name].isLog();
         const visible =
           this.showMinorTicks || !isLog || (isLog && !tick.isMinor);
