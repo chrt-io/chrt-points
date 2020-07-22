1d4c064f7b60a79b86f9d19cbf35f587ea4229ec
carlo <littleark@gmail.com>
2020-07-22 15:41:58 +0100

    added support for values between null values in line charts

--- src/charts/chrtLine/chrtLine.js
+++ src/charts/chrtLine/chrtLine.js
@@ -1,5 +1,5 @@
 import { isNull } from '~/helpers';
-import { createSVG as create,  } from '~/layout';
+import { createSVG as create } from '~/layout';
 import { lineWidth, lineColor } from './lib';
 import chrtGeneric from '../chrtGeneric';
 
@@ -14,17 +14,45 @@ function chrtLine() {
   this.stroke = DEAULT_LINE_COLOR;
 
   this.draw = () => {
-    if (!this.path) {
-      this.path = create('path');
-      this.g.appendChild(this.path);
-    }
-    if(!isNull(this._data)) {
+    if (!isNull(this._data)) {
+      if (!this.path) {
+        this.path = create('path');
+        this.g.appendChild(this.path);
+      }
+
       const d = this.interpolationFunction(this._data);
       this.path.setAttribute('d', d.join(''));
       this.path.setAttribute('fill', 'none');
       this.path.setAttribute('stroke', this.stroke);
-      this.path.setAttribute('stroke-width', this.strokeWidth)
-      this.path.setAttribute('stroke-linejoin', 'round')
+      this.path.setAttribute('stroke-width', this.strokeWidth);
+      this.path.setAttribute('stroke-linejoin', 'round');
+
+      const singlePoints = this._data.filter((d, i, points) => {
+        return (isNull(points[i - 1]) || isNull(points[i - 1][this.fields.y]))
+                &&
+                !isNull(d[this.fields.y])
+                &&
+                (isNull(points[i + 1]) || isNull(points[i + 1][this.fields.y]));
+      });
+
+      if (!this.points) {
+        this.points = [];
+        singlePoints.forEach((point) => {
+          const circle = create('circle');
+          this.points.push({
+            circle,
+            point,
+          });
+          this.g.appendChild(circle);
+        });
+      }
+
+      this.points.forEach(d => {
+        d.circle.setAttribute('cx', this.parentNode.scales['x'](d.point[this.fields.x]));
+        d.circle.setAttribute('cy', this.parentNode.scales['y'](d.point[this.fields.y]));
+        d.circle.setAttribute('fill', this.stroke);
+        d.circle.setAttribute('r', this.strokeWidth);
+      })
     }
 
     return this.parentNode;
@@ -37,7 +65,7 @@ chrtLine.parent = chrtGeneric.prototype;
 
 chrtLine.prototype = Object.assign(chrtLine.prototype, {
   width: lineWidth,
-  color: lineColor,
+  color: lineColor
 });
 
 export default chrtLine;
