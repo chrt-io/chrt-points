7563a9998f2321589774ae8ca2a3010cb79690df
carlo <littleark@gmail.com>
2021-09-01 22:32:54 +0100

    added anchorPoints for labels

--- src/chrtPoints.js
+++ src/chrtPoints.js
@@ -1,6 +1,13 @@
 import { isNull, isInfinity } from '~/helpers';
 import { createSVG as create } from '~/layout';
-import { pointSize, pointColor, pointStroke, pointStrokeWidth, pointOpacity, strokeOpacity } from './lib';
+import {
+  pointSize,
+  pointColor,
+  pointStroke,
+  pointStrokeWidth,
+  pointOpacity,
+  strokeOpacity
+} from './lib';
 import chrtGeneric from 'chrt-object';
 
 const DEFAULT_POINT_SIZE = 3;
@@ -20,22 +27,20 @@ function chrtPoints() {
   this._classNames = ['chrt-points'];
 
   this.getXScale = () => {
-    if(isNull(this.fields.x)) {
+    if (isNull(this.fields.x)) {
       this.fields.x = this.parentNode.scales.x[this.scales.x].field;
     }
     return this.parentNode.scales.x[this.scales.x];
-  }
+  };
 
   this.draw = () => {
-
     this._classNames.forEach(d => this.g.classList.add(d));
 
-    if(!isNull(this._data)) {
-
-      if(isNull(this.fields.x)) {
+    if (!isNull(this._data)) {
+      if (isNull(this.fields.x)) {
         this.fields.x = this.parentNode.scales.x[this.scales.x].field;
       }
-      if(isNull(this.fields.y)) {
+      if (isNull(this.fields.y)) {
         //console.log('this.scales', this.scales)
         //console.log('this.parentNode.scales', this.parentNode.scales)
         this.fields.y = this.parentNode.scales.y[this.scales.y].field;
@@ -49,25 +54,57 @@ function chrtPoints() {
           circle.setAttribute('data-id', `circle-${name}-${i}`);
           this.g.appendChild(circle);
         }
-        if(!isNull(this.parentNode.scales.x[this.scales.x]) && !isNull(this.parentNode.scales.y[this.scales.y])) {
-
+        if (
+          !isNull(this.parentNode.scales.x[this.scales.x]) &&
+          !isNull(this.parentNode.scales.y[this.scales.y])
+        ) {
           const x = this.parentNode.scales.x[this.scales.x](d[this.fields.x]);
           const y = this.parentNode.scales.y[this.scales.y](d[this.fields.y]);
-          circle.setAttribute('cx', !isNaN(x) && !isInfinity(x) ? x : 0);
-          circle.setAttribute('cy', !isNaN(y) && !isInfinity(y) ? y : 0);
-          circle.setAttribute('r', this.attr('radius')(d, i, arr));
+
+          const cx = !isNaN(x) && !isInfinity(x) ? x : 0;
+          const cy = !isNaN(y) && !isInfinity(y) ? y : 0;
+          const r = this.attr('radius')(d, i, arr);
+
+          d.anchorPoints = {
+            x: cx,
+            width: r,
+            y: cy,
+            height: r,
+            relativePosition: [0,-1],
+            directions: {
+              x: 1,
+              y: 1,
+            },
+            alignment: {
+              horizontal: 'middle',
+              vertical: 'top',
+            }
+          };
+
+          circle.setAttribute('cx', cx);
+          circle.setAttribute('cy', cy);
+          circle.setAttribute('r', r);
           circle.setAttribute('fill', this.attr('fill')(d, i, arr));
-          circle.setAttribute('fill-opacity', this.attr('fillOpacity')(d, i, arr) || 1);
+          circle.setAttribute(
+            'fill-opacity',
+            this.attr('fillOpacity')(d, i, arr) || 1
+          );
           circle.setAttribute('stroke', this.attr('stroke')(d, i, arr));
-          circle.setAttribute('stroke-width', this.attr('strokeWidth')(d, i, arr));
-          circle.setAttribute('stroke-opacity', this.attr('strokeOpacity')(d, i, arr));
+          circle.setAttribute(
+            'stroke-width',
+            this.attr('strokeWidth')(d, i, arr)
+          );
+          circle.setAttribute(
+            'stroke-opacity',
+            this.attr('strokeOpacity')(d, i, arr)
+          );
         }
       });
 
       // // // console.log('points', points);
     }
 
-    this.objects.forEach((obj) => obj.draw());
+    this.objects.forEach(obj => obj.draw());
 
     return this;
   };
@@ -87,7 +124,7 @@ chrtPoints.prototype = Object.assign(chrtPoints.prototype, {
   strokeWidth: pointStrokeWidth,
   opacity: pointOpacity,
   fillOpacity: pointOpacity,
-  strokeOpacity,
+  strokeOpacity
 });
 
 // export default chrtPoints;
