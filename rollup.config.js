d3a5c3957f697e49c0e7faf9f2b77171ddd23606
carlo <littleark@gmail.com>
2021-09-20 14:40:29 +0100

    removed rollup-plugin-uglify

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import commonjs from 'rollup-plugin-commonjs';
-import { uglify } from 'rollup-plugin-uglify';
 import resolve from 'rollup-plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import {terser} from "rollup-plugin-terser";
@@ -49,7 +48,6 @@ export default [
     },
     plugins: [
       ...config.plugins,
-      uglify(),
       terser({
         output: {
           preamble: config.output.banner
